3d111da0209fc5df76dd39aaa62117a0b805608c
Ilfat Galiev <zbritva@gmail.com>
2016-05-04 00:33:59 +0300

    Testing of spark env

--- src/SparkCubeRun.scala
+++ src/SparkCubeRun.scala
@@ -1,8 +1,18 @@
 /**
   * Created by zBritva on 04.05.16.
   */
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
 object SparkCubeRun {
   def main(args: Array[String]) {
-    print("HELLO!")
+    val sc = new SparkContext(master = "local[*]", appName = "SparkCubeRun", new SparkConf())
+
+    val data = Array(1, 2, 3, 4, 5)
+    val distData = sc.parallelize(data)
+
+    val result = distData.count()
+    print(result)
   }
 }
--- src/org/zbritva/spark/SparkCube.scala
+++ src/org/zbritva/spark/SparkCube.scala
@@ -1,3 +1,5 @@
+package org.zbritva.spark
+
 /**
   * Created by zBritva on 04.05.16.
   */
