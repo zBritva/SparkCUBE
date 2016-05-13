9ec35f8cc539f3f907ccec0de4e2bf76d1de0df4
Ilfat Galiev <zbritva@gmail.com>
2016-05-13 09:19:09 +0300

    Construct extension matrix for solve task by simplex method

--- src/org/zbritva/graph/BiparteGrapthMatching.scala
+++ src/org/zbritva/graph/BiparteGrapthMatching.scala
@@ -1,15 +1,49 @@
 package org.zbritva.graph
 
-class BiparteGrapthMatching{
-	def solve(edges_cost: Array[Array[Int]]){
-		// expand souece matrix with fictive vertex's (start and finish vertexts)
-		// var cols_count = edges_cost.length;
+class BiparteGrapthMatching {
+  def solve(edges_cost: Array[Array[Int]]) {
+    // expand souece matrix with fictive vertex's (start and finish vertexts)
+    // var cols_count = edges_cost.length;
+    //		val test = Array(
+    //			Array(1, 2, Int.MaxValue),
+    //			Array(Int.MaxValue, 1, 2),
+    //			Array(1, Int.MaxValue, 2)
+    //		)
 
-	}
+    //create new array with length of row. It is a pseudo row
+    val extension = new Array[Int](edges_cost(0).length)
 
-	def objective_function(edges_cost: Array[Array[Int]]){
+    //init values with zeros
+    //maybe it is not need
+    for( index <- extension.indices){
+      extension(index) = 1
+    }
 
-	}
+    //extend source matrix with new row
+    var edges_cost_ext = edges_cost :+ extension
+
+    //add psuedo colum
+    //foreach
+    for( index <- edges_cost_ext.indices){
+      //add item in end of each row for create new column
+      edges_cost_ext(index) = edges_cost_ext(index) :+ 1
+    }
+
+    val value = objective_function(edges_cost_ext)
+
+    print(value)
+  }
+
+  def objective_function(edges_cost: Array[Array[Int]]) : Int = {
+    val objective_function_nodes = edges_cost(edges_cost.length - 1).length - 1
+    var objective_function_value = 0
+
+    for( index <- edges_cost(objective_function_nodes).indices){
+      val value = edges_cost(objective_function_nodes)(index)
+      objective_function_value = objective_function_value + value
+    }
+
+    objective_function_value
+  }
 }
-
 
--- src/org/zbritva/main/SparkCubeRun.scala
+++ src/org/zbritva/main/SparkCubeRun.scala
@@ -1,11 +1,12 @@
 package org.zbritva.main
 
 /**
-* Created by zBritva on 04.05.16.
-*/
+  * Created by zBritva on 04.05.16.
+  */
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.log4j.{Level, Logger}
+import org.zbritva.graph.BiparteGrapthMatching
 
 object SparkCubeRun extends App {
 
@@ -14,10 +15,24 @@ object SparkCubeRun extends App {
     val conf = new SparkConf()
     val sc = new SparkContext(master = "local[*]", appName = "SparkCubeRun", conf)
 
+    //test spark
     val data = Array(1, 2, 3, 4, 5)
     val distData = sc.parallelize(data)
 
     val result = distData.count()
     print(result)
+
+    val graph_sover = new BiparteGrapthMatching()
+
+    Array(1.9, 2.9, 3.4, 3.5)
+
+    val test = Array(
+      Array(1, 2, Int.MaxValue),
+      Array(Int.MaxValue, 1, 2),
+      Array(1, Int.MaxValue, 2)
+    )
+
+    graph_sover.solve(test)
+
   }
 }
