30861437acde43c2d7c378b7f3b27a3a120b70a0
Ilfat Galiev <zbritva@gmail.com>
2016-05-27 19:10:03 +0300

    Determining level of each node of tree

--- src/org/zbritva/graph/tree/ExecutionTree.scala
+++ src/org/zbritva/graph/tree/ExecutionTree.scala
@@ -14,7 +14,7 @@ class ExecutionTree(root: TreeNode, level_list: Map[Int, immutable.Set[List[Stri
 
   var _root: TreeNode = root
   var _level_list: Map[Int, immutable.Set[List[String]]] = level_list
-
+  var _level_list_tree: Map[Int, mutable.Set[TreeNode]] = level_list_tree
 
   def getRoot(): TreeNode = {
     this._root
@@ -25,24 +25,24 @@ class ExecutionTree(root: TreeNode, level_list: Map[Int, immutable.Set[List[Stri
   }
 
   def _solveSimpexOptimizationTask(): Unit = {
-    val levels = _level_list.toList.sortBy(_._1)
-    for (level <- levels) {
-      if (level._1 == 0) {
-        //skip zero level, because do nothing
-
-      } else {
-        //other cases
-        val additionalCopies = level._1 - 1
-        val currentLevelNodesCount = level._2.size
-        val objectiveFunctionSize: Int = currentLevelNodesCount + currentLevelNodesCount * additionalCopies
-
-        val objectiveFunction = Array[Double](objectiveFunctionSize)
-        //fill objective function coefficients
-        for(functionValue <- level._2){
-
-        }
-
-      }
-    }
+//    val levels = _level_list_tree.toList.sortBy(_._1)
+//    for (level <- levels) {
+//      if (level._1 == 0) {
+//        //skip zero level, because do nothing
+//
+//      } else {
+//        //other cases
+//        val additionalCopies = level._1 - 1
+//        val currentLevelNodesCount = level._2.size
+//        val objectiveFunctionSize: Int = currentLevelNodesCount + currentLevelNodesCount * additionalCopies
+//
+//        val objectiveFunction = Array[Double](objectiveFunctionSize)
+//        //fill objective function coefficients
+//        for(functionValue <- level._2){
+//
+//        }
+//
+//      }
+//    }
   }
 }
